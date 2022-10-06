ac7290340434d62596e05486e9761ea7de34d000
Hifumisama <m.khayat92@gmail.com>
2022-10-06 12:03:18 +0200

    ajout de la base de prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  supabaseUrl: 'https://efuujxseobguwydbbglg.supabase.co',
+  supabaseUrl: 'https://qmzvuqjryceppuioixtl.supabase.co',
   supabaseKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVmdXVqeHNlb2JndXd5ZGJiZ2xnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjM1Nzg5ODAsImV4cCI6MTk3OTE1NDk4MH0.ZvCJ8SFRlJlXKpmv7LOTKK2T-eENqiz2ntgAEyBU2Ck',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtenZ1cWpyeWNlcHB1aW9peHRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjUwNDk4NDgsImV4cCI6MTk4MDYyNTg0OH0.hE71iaRSynW3SS39oe1w0rz52sfvmzbM1QcbKn5UFK0',
 };
